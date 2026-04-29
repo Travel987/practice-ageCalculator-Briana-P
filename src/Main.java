498450d082a13f57b1dc1798fc509d2f7cf2aaf6
Travel987 <brianapowers78@gmail.com>
2026-04-28 19:29:34 -0500

    Completed age calculator

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        int yearsLeft = 100 - age;
+
+        System.out.println("You have " + yearsLeft + " years until you are 100 years old.");
+    }
+}
